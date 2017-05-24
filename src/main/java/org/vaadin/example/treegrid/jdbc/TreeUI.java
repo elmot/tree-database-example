e4ab22f0affeb0b766c450dc6941ff8f6b9e7640
elmot <elmot@vaadin.com>
2017-05-24 13:53:00 +0300

    Companies have generated emails

--- src/main/java/org/vaadin/example/treegrid/jdbc/TreeUI.java
+++ src/main/java/org/vaadin/example/treegrid/jdbc/TreeUI.java
@@ -12,13 +12,13 @@ import com.vaadin.ui.Panel;
 import com.vaadin.ui.Tree;
 import com.vaadin.ui.TreeGrid;
 import com.vaadin.ui.UI;
+import org.vaadin.example.treegrid.jdbc.pojo.Company;
 import org.vaadin.example.treegrid.jdbc.pojo.Department;
 import org.vaadin.example.treegrid.jdbc.pojo.NamedItem;
 import org.vaadin.example.treegrid.jdbc.pojo.Person;
 
 import javax.servlet.annotation.WebServlet;
 import java.util.Objects;
-import java.util.function.Function;
 
 /**
  * This UI is the application entry point.
@@ -57,7 +57,7 @@ public class TreeUI extends UI {
 
         treeGrid.addColumn(ofPerson(Person::getFirstName)).setCaption("First Name");
         treeGrid.addColumn(ofPerson(Person::getLastName)).setCaption("Last Name");
-        treeGrid.addColumn(ofPerson(Person::getEmail)).setCaption("e-mail");
+        treeGrid.addColumn(new EmailGenerator()).setCaption("e-mail");
         treeGrid.addColumn(ofPerson(Person::getGender)).setCaption("Gender");
         treeGrid.setDataProvider(new PeopleData());
         return treeGrid;
@@ -76,7 +76,7 @@ public class TreeUI extends UI {
     public static class MyUIServlet extends VaadinServlet {
     }
 
-    private static ValueProvider<NamedItem, String> ofPerson(Function<Person, String> personExtractor) {
+    private static ValueProvider<NamedItem, String> ofPerson(ValueProvider<Person, String> personExtractor) {
         return (NamedItem item) -> {
             if (item instanceof Person) {
                 return personExtractor.apply((Person) item);
@@ -101,10 +101,22 @@ public class TreeUI extends UI {
                 }
             } else if (p instanceof Department) {
                 return VaadinIcons.GROUP;
-            }
-            else{
+            } else {
                 return VaadinIcons.OFFICE;
             }
+        }
+    }
+
+    public static class EmailGenerator implements ValueProvider<NamedItem, String> {
+
+        @Override
+        public String apply(NamedItem namedItem) {
+            if (namedItem instanceof Company) {
+                String domainName = namedItem.getName().replaceAll("^[A-Za-z0-9.-]", "").toLowerCase();
+                return "inbox@" + domainName + ".com";
+            };
+            if (namedItem instanceof Person) return ((Person) namedItem).getEmail();
+            return "--";
         }
     }
 }
